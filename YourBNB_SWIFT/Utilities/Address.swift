3527119aef60bb2746fb1608f6664cc8c31d6b4e
Nikhil Nikhil <nikhilthakur050@gmail.com>
2019-06-18 18:26:46 -0400

    [Nikhil] : Address

---
+++ YourBNB_SWIFT/Utilities/Address.swift
@@ -0,0 +1,16 @@
+//
+//  Address.swift
+//  YourBNB_SWIFT
+//
+//  Created by MacStudent on 2019-06-18.
+//  Copyright © 2019 Bikloo. All rights reserved.
+//
+
+import Foundation
+
+struct Address
+{
+    
+    var state : String
+    var suite : String
+}
