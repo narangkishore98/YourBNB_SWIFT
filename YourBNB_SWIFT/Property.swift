f264156042595487be3da022b6b9af0770b32c17
Kishore Narang <narangkishore98@gmail.com>
2019-06-18 18:25:57 -0400

    [Kishore] Adding The Variables in Property Class

--- YourBNB_SWIFT/Property.swift
+++ YourBNB_SWIFT/Property.swift
@@ -10,4 +10,23 @@ import Foundation
 class Property
 {
     var propertyID:String //CXXX or OXXX
+    var propertyName:String? //SkyView Manor if appartments not mandatory to give
+    var totalRooms:Int //represents number of rooms available in home
+    var maxPeopleAllowed:Int // represents maximum number of people allowed in home.
+    var pricePerPerson:Float // if only one person is comming what will be the price for individual
+    var totalPrice:Float // price for the whole property
+    var isAvailable:Bool // if the property is available for rent at the current time or not.
+    var address:Address
+    init(propertyID:String, address:Address, totalRooms:Int , maxPeopleAllowed:Int, pricePerPerson:Float, totalPrice:Float, isAvailable:Bool )
+    
+    {
+        self.propertyID = propertyID
+        self.address = address
+        self.totalRooms = totalRooms
+        self.maxPeopleAllowed = maxPeopleAllowed
+        self.isAvailable = isAvailable
+        self.pricePerPerson = pricePerPerson
+        self.totalPrice = totalPrice
+    
+    }
 }
