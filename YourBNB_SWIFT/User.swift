717fdd15fa6d47a532b6c5a21e289e1a56714722
Tarlochan <brotherstarlochan5268@gmail.com>
2019-06-21 18:24:24 -0400

    [Tarlochan] made users.json and enum gender in users

--- YourBNB_SWIFT/User.swift
+++ YourBNB_SWIFT/User.swift
@@ -8,11 +8,17 @@
 
 import Foundation
 
+enum Gender
+{
+    case Male,Female,Other
+}
+
 struct User
 {
     var userID:String // CXXX or OXXX depending upon user type
     var firstName:String //First Name
     var lastName:String // Last Name
+    var gender : Gender
     var password:String
     var fullName:String{
         return firstName + "  " + lastName
@@ -25,12 +31,13 @@ struct User
     
     // methods and behaviours of the users depend upon the user type and may throw some errors when user type is not compatible
   
-    init(userID:String ,firstName:String, lastName:String, email:String, mobile:Int64, password:String)
+    init(userID:String ,firstName:String, lastName:String, gender : Gender, email:String, mobile:Int64, password:String)
         
     {
         self.userID = userID
         self.firstName = firstName
         self.lastName = lastName
+        self.gender = gender
         self.email = email
         self.mobile = mobile
         self.password = password
